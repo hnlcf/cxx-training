0da5967e3f548eed9acc73d106d7533875c93cd5
Changfeng <louchangfeng@outlook.com>
2023-01-14 15:09:33 +0800

    Update gitignore

--- crimson/include/crimson/cn_pointer.h
+++ crimson/include/crimson/cn_pointer.h
@@ -13,15 +13,24 @@ struct cn_pointer_s
     cn_raw_ptr ptr;
 
     /// the memory length in bytes.
-    size_t size;
+    cn_usize size;
 };
 
-/// Initialize `size` bytes memory for fat pointer.
+/// Allocate `size` bytes memory and initialize it for fat pointer.
 cn_raw_ptr
-cn_pointer_init(cn_pointer_t *p, size_t size);
+cn_pointer_init(cn_pointer_t *p, cn_usize size);
 
 /// Free all memory owned by fat pointer.
 void
 cn_pointer_retake(cn_pointer_t *p);
+
+
+/// Compare two fat pointers with given cmp function.
+cn_bool
+cn_pointer_cmp(cn_pointer_t *p1, cn_pointer_t *p2, cn_cmp_fn cmp);
+
+/// Compare whether two dat pointers are equal(include memory on heap).
+cn_bool
+cn_pointer_is_equal(cn_pointer_t *p1, cn_pointer_t *p2);
 
 #endif // CN_POINTER_H_INCLUDE
